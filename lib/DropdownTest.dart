8fbcfb0f32b6335e94230fda3161d0aca0263fa2
Syed Muhammad Ali Aoun <80461232+AliAoun@users.noreply.github.com>
2024-04-12 02:30:20 +0500

    updated DropDownTest

--- lib/DropdownTest.dart
+++ lib/DropdownTest.dart
@@ -1,6 +1,11 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class DropDownTesting extends StatefulWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+
+
+import 'Models/PostsModel.dart';class DropDownTesting extends StatefulWidget {
   const DropDownTesting({super.key});
 
   @override
@@ -8,6 +13,25 @@ class DropDownTesting extends StatefulWidget {
 }
 
 class _DropDownTestingState extends State<DropDownTesting> {
+
+  List<PostsModel> postList = [];
+
+  Future<List<PostsModel>> getPostApi() async {
+    final response =
+    await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      postList.clear();
+      for (Map i in data) {
+        postList.add(PostsModel.fromJson(i));
+      }
+      return postList;
+    } else {
+      return postList;
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
