d92bd352ecc4f079da27e19e4d3b0f2ec52aa498
Syed Muhammad Ali Aoun <80461232+AliAoun@users.noreply.github.com>
2024-04-12 02:50:19 +0500

    updated DropDownTest

--- lib/DropdownTest.dart
+++ lib/DropdownTest.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -14,26 +15,51 @@ import 'Models/PostsModel.dart';class DropDownTesting extends StatefulWidget {
 
 class _DropDownTestingState extends State<DropDownTesting> {
 
-  List<PostsModel> postList = [];
+  Future<List<PostsModel>> getpost() async{
+    try{
+      final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+      final body = json.decode(response.body) as List;
 
-  Future<List<PostsModel>> getPostApi() async {
-    final response =
-    await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
-    var data = jsonDecode(response.body.toString());
-    if (response.statusCode == 200) {
-      postList.clear();
-      for (Map i in data) {
-        postList.add(PostsModel.fromJson(i));
+      if(response.statusCode == 200){
+        return body.map((e){
+          final map = e as Map<String, dynamic>;
+          return PostsModel(
+            id: map['id'],
+            userId: map['userId'],
+            title: map['title'],
+            body: map['body'],
+          );
+        }).toList();
       }
-      return postList;
-    } else {
-      return postList;
     }
+    on SocketException{
+      throw Exception('No Internet');
+    }
+    throw Exception('Error fetching data');
   }
 
 
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
+  }
+}
+
+
+
+List<PostsModel> postList = [];
+
+Future<List<PostsModel>> getPostApi() async {
+  final response =
+  await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+  var data = jsonDecode(response.body.toString());
+  if (response.statusCode == 200) {
+    postList.clear();
+    for (Map i in data) {
+      postList.add(PostsModel.fromJson(i));
+    }
+    return postList;
+  } else {
+    return postList;
   }
 }
